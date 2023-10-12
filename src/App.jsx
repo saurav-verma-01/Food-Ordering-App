625bb6cd775004ad2eb7bab36e393a3cfd735d55
Saurav kumar verma <sauravkumarverma22@gmail.com>
2023-10-13 01:30:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <div>
+        <h1>HEADER</h1>
+        <h2>LOGO</h2>
+        <h2>NAV</h2>
+      </div>
+      <div>
+        <h1>BODY</h1>
+        <h2>RESTRAUNT CONTAINER</h2>
+        <h3>RESTRAUNT CARDS</h3>
+      </div>
+      <div>
+        <h1>FOOTER</h1>
+      </div>
+    </div>
+  );
+};
+
+export default App;
